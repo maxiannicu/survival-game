7738f5edfe09e07373a77ee9d8a487fe4348dac4
Mihai Bunescu <marketgid01@gmail.com>
2017-07-29 22:44:03 +0300

    droids buying

--- Assets/Scripts/CapsuleStore.cs
+++ Assets/Scripts/CapsuleStore.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 using System;
 
 public static class CapsuleStore {
+	
 	#region Fields
 	private static int _capsuleCount = 0;
+	private const int _droidPrice = 1;
 	#endregion
 
 	#region Properties
@@ -27,8 +29,20 @@ public static class CapsuleStore {
 		if (Capsules >= MaxCapsules)
 			return;
 		
-		var oldBalance = _capsuleCount;
 		_capsuleCount++;
 	}
+
+	public static bool CanBuyDroid(){
+		return Capsules > _droidPrice;
+	}
+
+	public static void BuyDroid(){
+		if (!CanBuyDroid ()) {
+			throw new Exception ("Not enoght capsules");
+		}
+
+		_capsuleCount -= _droidPrice;
+	}
+
 	#endregion
 }
