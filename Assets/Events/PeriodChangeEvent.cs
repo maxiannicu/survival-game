43d2cb3553e340ff27a57cf49375614db9512622
Nicu Maxian <nicu.maxian@infigosoftware.com>
2017-07-29 17:15:22 +0300

    Added period controller

---
+++ Assets/Events/PeriodChangeEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PeriodChangeEvent : EventArgs {
+	public PeriodChangeEvent(Period period){
+		CurrentPeriod = period;
+	}
+
+	public Period CurrentPeriod {
+		get;
+		private set;
+	}
+}
---
+++ Assets/Scripts/PeriodController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PeriodController : MonoBehaviour {
+	#region Fields
+	private float _ellapsedTime;
+	public int PeriodTime = 20;
+	#endregion
+
+	#region Events
+	public event EventHandler<PeriodChangeEvent> PeriodChangeEvent;
+	#endregion
+
+	#region Properties
+	public Period CurrentPeriod {
+		get;
+		private set;
+	}
+	#endregion
+
+	#region Methods
+
+	public void Start(){
+		CurrentPeriod = Period.Day;
+	}
+
+	public void Update(){
+		_ellapsedTime += Time.deltaTime;
+
+		if (_ellapsedTime >= PeriodTime) {
+			CurrentPeriod = (CurrentPeriod == Period.Day) ? Period.Night : Period.Day;
+			_ellapsedTime = 0;
+
+			PeriodChangeEvent (this,new PeriodChangeEvent(CurrentPeriod));
+		}
+	}
+	#endregion
+}
