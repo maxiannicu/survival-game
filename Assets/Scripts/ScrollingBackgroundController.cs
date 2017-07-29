8d696ef2b9d84928095c7f4e948ef79aa43ea9ae
Mihai Bunescu <marketgid01@gmail.com>
2017-07-29 19:20:43 +0300

    scrolling background

---
+++ Assets/Scripts/ScrollingBackgroundController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackgroundController : MonoBehaviour {
+	public float backgroundSize;
+	public float paralaxSpeed;
+
+	private Transform cameraTransforms;
+	private Transform[] layers;
+	private float viewZone = 1f;
+	private int leftIndex;
+	private int rightIndex;
+	private float lastCameraX;
+
+	private void Start(){
+		cameraTransforms = Camera.main.transform;
+		lastCameraX = cameraTransforms.position.x;
+		layers = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++) {
+			layers [i] = transform.GetChild(i);
+		}
+
+		leftIndex = 0;
+		rightIndex = layers.Length - 1;
+	}
+
+	private void Update() {
+		float deltaX = cameraTransforms.position.x - lastCameraX;
+		transform.position  += Vector3.right * (deltaX * paralaxSpeed);
+		lastCameraX = cameraTransforms.position.x;
+
+		if (cameraTransforms.position.x < (layers[leftIndex].transform.position.x + viewZone))
+			ScrollLeft();
+		if (cameraTransforms.position.x > (layers[rightIndex].transform.position.x - viewZone))
+			ScrollRight ();
+	}
+
+
+	private void ScrollLeft() {
+		int lastRight = rightIndex;
+		layers[rightIndex].position = Vector3.right * (layers [leftIndex].position.x - backgroundSize);
+		leftIndex = rightIndex;
+		rightIndex--;
+		Debug.Log (rightIndex);
+		if (rightIndex < 0) {
+			rightIndex = layers.Length - 1;
+		}
+	}
+
+	private void ScrollRight() {
+		int lastLeft = leftIndex;
+		layers[leftIndex].position = Vector3.right * (layers [rightIndex].position.x + backgroundSize);
+		rightIndex = leftIndex;
+		leftIndex++;
+		if (leftIndex == layers.Length) {
+			leftIndex = 0;
+		}
+	}
+}
