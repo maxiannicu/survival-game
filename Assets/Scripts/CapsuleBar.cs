25b7f9cd043f02719b06e840df963514fd33535e
Nicu Maxian <nicu.maxian@infigosoftware.com>
2017-07-29 22:55:17 +0300

    Changed progress bar

--- Assets/Scripts/CapsuleBar.cs
+++ Assets/Scripts/CapsuleBar.cs
@@ -1,24 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CapsuleBar : MonoBehaviour {
-	public Texture2D Background;
-	public Texture2D Fill;
-	public Vector2 Position = new Vector2(40,40);
-	public Vector2 Size = new Vector2(400,50);
+	private Image _image;
 
-	public void OnGUI() {
-		GUI.BeginGroup(new Rect(Position.x, Position.y, Size.x, Size.y));
-		GUI.DrawTexture(new Rect(0,0, Size.x, Size.y),Background);
-	
-		var width = Size.x / CapsuleStore.MaxCapsules;
-		var height = Size.y;
-		for (int i = 0; i < CapsuleStore.Capsules; i++) {
-			var rect = new Rect (i * width, 0, width, height);
-			GUI.DrawTexture(rect ,Fill);
-		}
+	public void Start(){
+		_image = gameObject.GetComponent<Image> ();
+	}
 
-		GUI.EndGroup();
+	public void Update() {
+		_image.fillAmount = CapsuleStore.Capsules * 1.0f / CapsuleStore.MaxCapsules;
 	}
 }
