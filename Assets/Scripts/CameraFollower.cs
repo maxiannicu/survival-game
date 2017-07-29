5ecac7beed77f08ecd8df0cf376331da6f876d11
Nicu Maxian <nicu.maxian@infigosoftware.com>
2017-07-29 15:54:52 +0300

    Added basics

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+	public GameObject follower;
+		
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = new Vector3 (
+			follower.transform.position.x,
+			follower.transform.position.y,
+			gameObject.transform.position.z
+		);
+	}
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour {
+	public Rigidbody2D body;
+	public float velocity = 0.5f;
+	public float jumpForce = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	void Update () {
+		DirectionUpdate ();
+	}
+
+	private void DirectionUpdate(){
+		var direction = Input.GetAxis ("Horizontal");
+
+		body.transform.position = new Vector2 (
+			body.transform.position.x + (velocity * direction * Time.deltaTime),
+			body.transform.position.y
+		);
+	}
+}
