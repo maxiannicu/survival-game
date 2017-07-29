b0a27be12769a90149ff683947514f6e86a89974
Nicu Maxian <nicu.maxian@infigosoftware.com>
2017-07-29 16:47:42 +0300

    Added events for Capsules

---
+++ Assets/Events/CapsuleBalanceChanged.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CapsuleBalanceChanged : EventArgs {
+
+	public CapsuleBalanceChanged(int oldBallance,int newBallance){
+		this.OldBallance = oldBallance;
+		this.NewBallance = newBallance;
+	}
+
+	public int NewBallance {
+		get;
+		private set;
+	}
+
+	public int OldBallance {
+		get;
+		private set;
+	}
+}
---
+++ Assets/Scripts/CapsuleStore.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CapsuleStore : MonoBehaviour {
+	#region Fields
+	private int _capsuleCount = 0;
+	#endregion
+
+	#region Events
+	public event EventHandler<CapsuleBalanceChanged> CapsuleChangeEvent;
+	#endregion
+
+	#region Properties
+	public int Capsules {
+		get {
+			return _capsuleCount;
+		}
+	}
+
+	public int MaxCapsules {
+		get {
+			return 100;
+		}
+	}
+	#endregion
+
+	#region Methods
+	public void AddCapsule(){
+		var oldBalance = _capsuleCount;
+		_capsuleCount++;
+		CapsuleChangeEvent (this, new CapsuleBalanceChanged (oldBalance,_capsuleCount));
+	}
+	#endregion
+}
