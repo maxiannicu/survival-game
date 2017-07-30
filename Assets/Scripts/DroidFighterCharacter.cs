02339a752ffbda1f4a5c3c391345c6e9e0be02c2
SchiduLuca <schiduluca@gmail.com>
2017-07-30 18:50:00 +0300

    is trigger added

--- Assets/Scripts/DroidFighterCharacter.cs
+++ Assets/Scripts/DroidFighterCharacter.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DroidFighterCharacter : MonoBehaviour {
+public class DroidFighterCharacter : AbstractCharacter {
+	private UpgradableComponent _upgradableComponent;
+	private int direction;
 
 	// Use this for initialization
 	void Start () {
