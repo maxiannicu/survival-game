59dabb14a87d98d7f32a14cb358e33c1a6019266
Nicu Maxian <nicu.maxian@infigosoftware.com>
2017-07-30 13:45:48 +0300

    Added price display

---
+++ Assets/Scripts/CapsulePriceRender.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsulePriceRender : MonoBehaviour {
+	private int _count = 0;
+	public Texture Capsule;
+	public float OffsetBetweelCapsules;
+	public float Y;
+	public float XCenter;
+
+	public void Start(){
+		DeActivate ();
+	}
+
+	public void Activate(int count){
+		this._count = count;
+		enabled = true;
+	}
+
+	public void DeActivate(){
+		this._count = 0;
+		enabled = false;
+	}
+
+	public void OnGUI() 
+	{
+		if(this._count > 0){
+			var occupedWidth = this._count * Capsule.width + ((this._count - 1) * OffsetBetweelCapsules);
+			var startingX = XCenter - occupedWidth/2;
+			for (int i = 0; i < this._count; i++) {
+				GUI.DrawTexture (new Rect(startingX,Y, Capsule.width, Capsule.height), Capsule);
+				startingX += Capsule.width;
+
+				if (i < this._count - 1) {
+					startingX += OffsetBetweelCapsules;
+				}
+			}
+
+		}
+	}
+}
