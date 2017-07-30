fc9cd4bb9dc6a5582c047dd4ac9f2cfe7fa1cfd5
Nicu Maxian <nicu.maxian@infigosoftware.com>
2017-07-30 19:20:03 +0300

    Changed purchase

--- Assets/Scripts/CapsulePriceRender.cs
+++ Assets/Scripts/CapsulePriceRender.cs
@@ -4,36 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CapsulePriceRender : MonoBehaviour {
-	private int _count = 0;
-	public Texture Capsule;
+	private int _active = 0;
+	private int _total = 0;
+
+	public Texture CapsuleEmpty;
+	public Texture CapsuleFill;
 	public float OffsetBetweelCapsules;
 	public float Y;
 	public float XCenter;
 
 	public void Start(){
-		DeActivate ();
+		Hide ();
 	}
 
-	public void Activate(int count){
-		this._count = count;
+	public void Show(int active,int total){
+		this._active = active;
+		this._total = total;
+		Debug.Log("Showing "+active+"/"+total);
 		enabled = true;
 	}
 
-	public void DeActivate(){
-		this._count = 0;
+	public void Hide(){
+		this._active = 0;
 		enabled = false;
 	}
 
 	public void OnGUI() 
 	{
-		if(this._count > 0){
-			var occupedWidth = this._count * Capsule.width + ((this._count - 1) * OffsetBetweelCapsules);
+		if(this._total > 0){
+			var occupedWidth = this._total * CapsuleEmpty.width + ((this._total - 1) * OffsetBetweelCapsules);
 			var startingX = XCenter - occupedWidth/2;
-			for (int i = 0; i < this._count; i++) {
-				GUI.DrawTexture (new Rect(startingX,Y, Capsule.width, Capsule.height), Capsule);
-				startingX += Capsule.width;
 
-				if (i < this._count - 1) {
+			for (int i = 0; i < this._total; i++) {
+				var texture = i < this._active ? CapsuleFill : CapsuleEmpty;
+				GUI.DrawTexture (new Rect(startingX,Y, texture.width, texture.height), texture);
+				startingX += texture.width;
+
+				if (i < this._total - 1) {
 					startingX += OffsetBetweelCapsules;
 				}
 			}
--- Assets/Scripts/PurchaseController.cs
+++ Assets/Scripts/PurchaseController.cs
@@ -4,29 +4,40 @@ using UnityEngine;
 using System;
 
 public class PurchaseController : MonoBehaviour {
+	private System.Diagnostics.Stopwatch _stopWatch = new System.Diagnostics.Stopwatch();
 	private UpgradableComponent _upgradableComponent = null;
 	public CapsulePriceRender CapsulePriceRender;
 	public TooltipRender TooltipRender;
 
 	public void Update(){
-		if (Input.GetKeyUp (KeyCode.DownArrow)) {
+		if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			_stopWatch.Start ();
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
 			if (_upgradableComponent != null && _upgradableComponent.IsUpgradable) {
-				try {
-					_upgradableComponent.Upgrade ();
-					ShowPrice ();
-					Debug.Log ("Purchased");
-				} catch (Exception ex) {
-					Debug.Log ("Could not upgrade, cause : " + ex.Message);
+				if (_stopWatch.ElapsedMilliseconds >= _upgradableComponent.UpgradePrice * Constants.Game.MilliSecondsToBuyOneCapsule) {
+					try {
+						_upgradableComponent.Upgrade ();
+						_stopWatch.Stop ();
+						Debug.Log ("Purchased");
+					} catch (Exception ex) {
+						Debug.Log ("Could not upgrade, cause : " + ex.Message);
+					}
 				}
+				ShowPrice ();
 			}
+		}
+
+		if (Input.GetKeyUp (KeyCode.DownArrow)) {
+			_stopWatch.Stop ();
 		}
 	}
 
 	public void OnTriggerEnter2D(Collider2D collision) {
 		_upgradableComponent = collision.GetComponent<UpgradableComponent> ();
 
-		if (_upgradableComponent != null) {
-			
+		if (_upgradableComponent != null) {			
 			Debug.Log ("Found upgradable component");
 			ShowPrice ();
 		}
@@ -42,7 +53,8 @@ public class PurchaseController : MonoBehaviour {
 
 	private void ShowPrice(){
 		if (_upgradableComponent.IsUpgradable) {
-			CapsulePriceRender.Activate (_upgradableComponent.UpgradePrice);
+			var progress = (int)Math.Truncate(1.0f * _stopWatch.ElapsedMilliseconds / Constants.Game.MilliSecondsToBuyOneCapsule);
+			CapsulePriceRender.Show (progress, _upgradableComponent.UpgradePrice);
 			switch (_upgradableComponent.UpgradeType) {
 			case UpgradableComponent.Type.Activable:
 				TooltipRender.ShowActivateTooltip ();
@@ -51,14 +63,13 @@ public class PurchaseController : MonoBehaviour {
 				TooltipRender.ShowUpgradeTooltip ();
 				break;
 			}
-
 		} else {
 			HidePrice ();
 		}
 	}
 
 	private void HidePrice(){
-		CapsulePriceRender.DeActivate ();
+		CapsulePriceRender.Hide ();
 		TooltipRender.HideTooltip ();
 	}
 }
